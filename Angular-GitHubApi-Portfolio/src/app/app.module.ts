ac6a11c3a1a54605642d471fb3424a1248c340c0
rtj-meanstack <rtj113@gmail.com>
2019-07-07 13:08:34 -0700

    Generated Components for Pages and Routing
    I generated the Home, Contact and Page not found pages. I also configured the angular routing. If a route isn't defined it will redirect to the PageNotFoundComponent. I also defined the default route of HomeComponent. I also generated a few shared components that will be within the Core folder in my project. These are the NavBar and Footer components. I also imported the Bootstrap CDN so I can start styling my pages.

--- Angular-GitHubApi-Portfolio/src/app/app.module.ts
+++ Angular-GitHubApi-Portfolio/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ContactComponent } from './modules/contact/pages/contact/contact.component';
+import { PageNotFoundComponent } from './modules/page-not-found/pages/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
