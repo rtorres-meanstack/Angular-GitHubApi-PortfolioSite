a7372e44a6a99252f8cee837e72de2de6379f035
rtj-meanstack <rtj113@gmail.com>
2019-08-05 22:27:55 -0700

    Modifying server.js
    Re-writing some of the connection scripts

--- api/server.js
+++ api/server.js
@@ -1,36 +1,47 @@
 const express = require('express'),
 // const dotenv = require('dotenv'),
-    dotenv = require('dotenv').config(),
-    var path = require('path'),
-    bodyParser = require('body-parser'),
-    cors = require('cors'),
-    mongoose = require('mongoose'),
-    config = require('./DB');
-    
-    /// Serve only the static files form the dist directory    
-    app.use(express.static(__dirname + '/dist'));
 
-    app.get('/*', function(req,res) {  
-        res.sendFile(path.join(__dirname+'/dist/index.html'));   
-    }); 
+const http = require('http');
+const path = require('path');
+const app = express();
 
-    const taskRoute = require('./routes/task.route');
-    mongoose.Promise = global.Promise;
+dotenv = require('dotenv').config(),
+// var path = require('path'),
+bodyParser = require('body-parser'),
+cors = require('cors'),
+mongoose = require('mongoose'),
+config = require('./DB');
 
-    // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/task", { useNewUrlParser: true }).then(
-    mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/task", { useNewUrlParser: true }).then(
-        () => { console.log('Database is connected') },
-        err => { console.log('Can not connect to the database' + err)}
-    );
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist'));
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(cors());
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
-    app.use('/task', taskRoute)
-    
-    const port = process.env.PORT || 8080;
+const taskRoute = require('./routes/task.route');
+mongoose.Promise = global.Promise;
 
-    const server = app.listen(port, () => {
-        console.log('Listening on port ' + port);
-    });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/task", { useNewUrlParser: true }).then(
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/task", { useNewUrlParser: true }).then(
+    () => { console.log('Database is connected') },
+    err => { console.log('Can not connect to the database' + err)}
+);
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/task', taskRoute)
+
+const port = process.env.PORT || 8080;
+
+app.set(port);
+const server = http.createServer(app);
+
+const server = app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
