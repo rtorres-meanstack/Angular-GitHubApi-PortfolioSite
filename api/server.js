622a1e6a7546a7327503351ccddac44a56d3aec1
rtj-meanstack <rtj113@gmail.com>
2019-08-06 21:24:33 -0700

    Server.js update
    Minor errors on the server, its not finding the dist file.

--- api/server.js
+++ api/server.js
@@ -18,9 +18,9 @@ config = require('./DB');
 // res.sendFile(path.join(__dirname, '/dist/index.html'));
 // });
 
-app.use(express.static(path.join(__dirname, '/dist/')));
+app.use(express.static(path.join(__dirname, '/dist')));
 app.get("/", function(req, res) {
-res.sendFile(path.join(__dirname + "/dist/"));
+res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 // app.get('/*', function(req, res) {
