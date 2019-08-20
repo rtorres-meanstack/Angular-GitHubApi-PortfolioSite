5fab827c1fad6578bb314594a384b871046a64e5
rtj-meanstack <rtj113@gmail.com>
2019-08-20 00:00:17 -0700

    Changed the task service route

--- src/app/modules/task-manager/service/tasks.service.ts
+++ src/app/modules/task-manager/service/tasks.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TasksService {
 
-  uri = 'http://localhost:8080/tasks';
+  uri = '/tasks';
 
   constructor(private http: HttpClient) { }
 
